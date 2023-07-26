694ca84970ccdd54704dce0e3e7c352170bdb274
Michael Feng <130256997+michaelfenggg@users.noreply.github.com>
2023-07-26 16:30:24 -0400

    Update display.py

--- display.py
+++ display.py
@@ -24,7 +24,6 @@ def countdown():
     if timer <= 20:
         label.config(text=f"{timer:02} sec", font = ("Inter", 35))
         timer += 1
-        progress.set_value(timer / 20)
         root.after(1000, countdown)
 
 timer = 0
@@ -32,9 +31,22 @@ label = tk.Label(root, text = f"{timer:02}")
 label.configure(bg = "white")
 label.place(relx = 1, rely = 1, anchor = "se")
 
-progress = tk_tools.RotaryScale(root, max_value = 7, size = 100, unit = "progress")
-progress.place(relx = 1, rely = 0.5, anchor = "w")
-progress.set_value(0)
+procedures = ["Palm to palm", "Palm over dorsum, fingers interlaced", "Palm to palm, fingers interlaced",
+ "Backs of fingers to opposing palm, fingers interlocked", "Rotational rubbing of the thumb", "Fingertips to palm",
+ "Turning off the faucet with a paper towel"]
+
+listbox = Listbox(root, height = 7, width = 30, bg = "white", font = ("Inter", 15), fg = "black")
+listbox.insert(1, "Palm to palm")
+listbox.insert(2, "Palm over dorsum, fingers interlaced")
+listbox.insert(3, "Palm to palm, fingers interlaced")
+listbox.insert(4, "Backs of fingers to opposing palm, fingers interlocked")
+listbox.insert(5, "Rotational rubbing of the thumb")
+listbox.insert(6, "Fingertips to palm")
+listbox.insert(7, "Turning off the faucet with a paper towel")
+
+def update(ind):
+    listbox.delete(ind)
+    listbox.insert(ind + 1, procedures[ind] + " —— DONE")
 
 countdown()
 print("Matplotlib Version : {}".format(matplotlib.__version__))
